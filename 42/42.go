a4462034ad852306dd777529812264db46f3de59
s444v <dymnich02@gmail.com>
2025-04-03 19:58:09 +0300

    03.04.2025

---
+++ 42/42.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handleTime(res http.ResponseWriter, req *http.Request) {
+	s := time.Now().Format("02.01.2006 15:04:05")
+	res.Write([]byte(s))
+}
+
+func handleDate(res http.ResponseWriter, req *http.Request) {
+	s := time.Now().Format("02.01.06")
+	res.Write([]byte(s))
+}
+
+func handleMain(res http.ResponseWriter, req *http.Request) {
+	s := fmt.Sprintf("Method: %s\nHost: %s\nPath: %s",
+		req.Method, req.Host, req.URL.Path)
+	res.Write([]byte(s))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/time", handleTime)
+	mux.HandleFunc("/date", handleDate)
+	mux.HandleFunc("/", handleMain)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
